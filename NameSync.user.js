21b95e8982fb780b6ad7eea6bbec2c27c299c20f
Macil Tech <maciltech@gmail.com>
2012-07-05 09:26:52 -0600

    Only pay attention to new posts, don't worry about inline and hover previews.

--- NameSync.user.js
+++ NameSync.user.js
@@ -479,13 +479,13 @@ function NameSync() {
 	
 	document.body.addEventListener('DOMNodeInserted', function(e) {
 		if (e.target.nodeName=='DIV') {
-			if ($jq(e.target).hasClass("replyContainer") && !$jq(e.target).hasClass("inline")) {
+			if ($jq(e.target).hasClass("replyContainer") && !$jq(e.target).parent().is(".inline, #qp")) {
 				updatePost($jq(".reply", e.target));
 				if (!delaySyncHandler) {
 					delaySyncHandler = setTimeout(function() {
-						setTimeout(sync, 4500, true);
+						setTimeout(sync, 3500, true);
 						delaySyncHandler = null;
-					}, 500);
+					}, 1500);
 				}
 			}
 				
