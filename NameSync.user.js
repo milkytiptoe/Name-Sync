da0e47c4dc07418b202790e465dd751ee591052b
milkytiptoe <directxpert@gmail.com>
2013-03-09 16:06:25 +1100

    @homepage seems unecessary

--- NameSync.user.js
+++ NameSync.user.js
@@ -11,7 +11,6 @@
 // @include        *://boards.4chan.org/soc/*
 // @updateURL      https://github.com/milkytiptoe/Name-Sync/raw/master/NameSync.user.js
 // @downloadURL    https://github.com/milkytiptoe/Name-Sync/raw/master/NameSync.user.js
-// @homepage       http://milkytiptoe.github.com/Name-Sync/
 // @icon           data:image/gif;base64,R0lGODlhIAAgAMQQABAQEM/Pz9/f3zAwMH9/f+/v7yAgIGBgYJ+fn6+vr4+Pj1BQUHBwcL+/v0BAQAAAAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAABAALAAAAAAgACAAAAXNICSOZGmeaKqubOu6QvC+DSDPopAQfFOMjYdthhg8jsiHowEJKmGOpPToUFBbAcB0i3SwBNqkYUE4GLbeVRRpQJQaxmQ6lUgOfqKDYx/vqpEAeCJZXHMnAkkEJoRThiYISYIkAg2Vlg03OJqbnC8MDgcEbikBew5hQpkjBUkMKk5TQyQESaomsLECQHYruA8DTCUIqA/BKb4PBgpMAghrSAcsyFxIAy1OBsRcB5LHVAIH1AYJLwJGaQIEDmdKB+Q4BQMLnSkF7/T4+fr4IQA7
 // ==/UserScript==
 
