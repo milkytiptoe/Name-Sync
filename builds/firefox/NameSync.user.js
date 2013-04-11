3f8f7b555667bb341fe014f2bbf4414ef45acdfb
milkytiptoe <directxpert@gmail.com>
2013-04-12 03:29:19 +1000

    4.0.3

--- builds/firefox/NameSync.user.js
+++ builds/firefox/NameSync.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan X Name Sync
-// @version      4.0.2
+// @version      4.0.3
 // @namespace    milky
 // @description  Enables names on 4chan's forced anon boards. Requires 4chan X v3.
 // @author       milkytiptoe
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 /*
-  4chan X Name Sync v4.0.2
+  4chan X Name Sync v4.0.3
   http://www.milkyis.me/
   
   Developers: milkytiptoe and ihavenoface
@@ -35,7 +35,7 @@
 
   g = {
     NAMESPACE: "NameSync.",
-    VERSION: '4.0.2',
+    VERSION: '4.0.3',
     threads: [],
     board: null
   };
