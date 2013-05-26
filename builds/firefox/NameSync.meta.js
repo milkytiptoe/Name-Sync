d537500033a4940bc37154da33039b300d682ef7
milkytiptoe <milky@namesync.org>
2013-05-27 00:47:12 +1000

    4.4.0

--- builds/firefox/NameSync.meta.js
+++ builds/firefox/NameSync.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan X Name Sync
-// @version      4.3.0
+// @version      4.4.0
 // @namespace    milky
 // @description  Enables names on 4chan's forced anon boards. Requires 4chan X.
 // @author       milkytiptoe
--- builds/firefox/NameSync.user.js
+++ builds/firefox/NameSync.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan X Name Sync
-// @version      4.3.0
+// @version      4.4.0
 // @namespace    milky
 // @description  Enables names on 4chan's forced anon boards. Requires 4chan X.
 // @author       milkytiptoe
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 /*
-  4chan X Name Sync v4.3.0
+  4chan X Name Sync v4.4.0
   https://www.namesync.org/
   
   Developers: milkytiptoe and ihavenoface
@@ -35,7 +35,7 @@
 
   g = {
     NAMESPACE: 'NameSync.',
-    VERSION: '4.3.0',
+    VERSION: '4.4.0',
     threads: []
   };
 
@@ -136,7 +136,7 @@
       url += "?" + data;
     }
     r.open(type, url, true);
-    r.setRequestHeader('X-Requested-With', 'NameSync4.3.0');
+    r.setRequestHeader('X-Requested-With', 'NameSync4.4.0');
     if (file === 'qp') {
       r.setRequestHeader('If-Modified-Since', Sync.lastModified);
     }
