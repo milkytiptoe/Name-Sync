77e3c75667116a1357f5fb08d44444ce8bd4637e
milkytiptoe <milky@namesync.org>
2013-05-28 09:36:43 +1000

    4.4.2

--- builds/firefox/NameSync.meta.js
+++ builds/firefox/NameSync.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         4chan X Name Sync
-// @version      4.4.1
+// @version      4.4.2
 // @namespace    milky
 // @description  Enables names on 4chan's forced anon boards. Requires 4chan X.
 // @author       milkytiptoe
