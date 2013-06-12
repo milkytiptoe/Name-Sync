54cd3d1d67ad484b798faafba9e95bea7c2326ed
milkytiptoe <milky@namesync.org>
2013-06-13 05:34:12 +1000

    license

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 For installation [click here](http://milkytiptoe.github.com/Name-Sync/)
 
 For support and more information [click here](https://github.com/milkytiptoe/Name-Sync/wiki/Support)
+
+[License](https://github.com/milkytiptoe/Name-Sync/blob/master/license)
--- src/banner.js
+++ src/banner.js
@@ -1,11 +1,13 @@
 /*
   <%= meta.name %> v<%= version %>
   <%= meta.page %>
-  
+
   Developers: milkytiptoe and ihavenoface
-  
+
   Contributers: https://github.com/milkytiptoe/Name-Sync/graphs/contributors
-  
+
+  License: https://github.com/milkytiptoe/Name-Sync/blob/master/license
+
   This script contains code from 4chan X (https://github.com/MayhemYDG/4chan-x)
   @license: https://github.com/MayhemYDG/4chan-x/blob/v3/LICENSE
 */
