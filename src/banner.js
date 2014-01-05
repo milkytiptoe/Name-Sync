262b8718350ec0eee2d70a9109b323f75793a00a
milky <milky@namesync.org>
2014-01-05 19:48:17 +1100

    Updates.

--- src/banner.js
+++ src/banner.js
@@ -2,11 +2,18 @@
   <%= meta.name %> v<%= version %>
   <%= meta.page %>
 
-  Developers: milkytiptoe and ihavenoface
+  Developers:
+  <%=
+  meta.authors.map(function(author) {
+    return author
+  }).join('\n  ')
+  %>
 
-  Contributers: https://github.com/milkytiptoe/Name-Sync/graphs/contributors
+  Contributers:
+  https://github.com/milkytiptoe/Name-Sync/graphs/contributors
 
-  License: https://raw.github.com/milkytiptoe/Name-Sync/master/license
+  License:
+  https://raw.github.com/milkytiptoe/Name-Sync/master/license
 
   This script contains code from 4chan X (https://github.com/MayhemYDG/4chan-x)
   @license: https://github.com/MayhemYDG/4chan-x/blob/v3/LICENSE
