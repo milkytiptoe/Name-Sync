28261c05cce31ff5e9886b18669a39e54b3c5c10
milky <milky@namesync.org>
2014-01-31 11:32:49 +1100

    meta update.

--- src/metadata.js
+++ src/metadata.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         <%= meta.name %>
 // @version      <%= version %>
-// @minGMVer     1.12
-// @minFFVer     22
+// @minGMVer     1.14
+// @minFFVer     26
 // @namespace    milky
 // @description  <%= description %>
 // @license      CC BY-NC-SA 3.0; https://raw.github.com/milkytiptoe/Name-Sync/master/license
